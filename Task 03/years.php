69f8d52a76e86134c41625638cb936350be86624
gourav-ravi <145653975+gourav-ravi@users.noreply.github.com>
2023-09-29 05:42:28 +0000

    made some changes

--- Task 03/years.php
+++ Task 03/years.php
@@ -1,7 +1,7 @@
 <?php
     //Finding number of days
     $date2 = strtotime('now'); 
-    $date1 = strtotime('2022-04-01');
+    $date1 = strtotime('1969-04-01');
     $diff = $date2 - $date1; 
     $days = floor($diff / (60 * 60 * 24));
     // $month = floor($diff /)
